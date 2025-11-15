2c1912958d4548b463596d6446af60b0b3c3c000
Lorot <62335161+3Lord3@users.noreply.github.com>
2025-11-15 16:26:05 +0300

    build: add vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import tailwindcss from "@tailwindcss/vite";
+import react from '@vitejs/plugin-react'
 import path from "path";
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), tailwindcss(), tsconfigPaths()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
+    server: {
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3001',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
 });
